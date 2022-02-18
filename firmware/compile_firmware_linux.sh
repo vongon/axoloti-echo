13e9773416aadbcc824434b3c18096607737af97
Tyler Coy <tylercoy@utexas.edu>
2022-02-18 14:43:50 -0600

    Use parallel processing for firmware compilation

--- firmware/compile_firmware_linux.sh
+++ firmware/compile_firmware_linux.sh
@@ -2,17 +2,17 @@
 export PATH=${axoloti_runtime}/platform_linux/bin:$PATH
 
 cd "${axoloti_firmware}"
-make -f Makefile.patch clean
+make -j$(nproc) -f Makefile.patch clean
 
 echo "Compiling bootloader..."
 cd bootloader
-make
+make -j$(nproc)
 cd ..
 
 echo "Compiling firmware... ${axoloti_firmware}"
 mkdir -p build/obj
 mkdir -p build/lst
-if ! make $1 ; then
+if ! make -j$(nproc) $1 ; then
     exit 1
 fi
 
@@ -20,12 +20,12 @@ echo "Compiling firmware flasher..."
 cd flasher
 mkdir -p flasher_build/lst
 mkdir -p flasher_build/obj
-make $1
+make -j$(nproc) $1
 cd ..
 
 echo "Compiling firmware mounter..."
 cd mounter
 mkdir -p mounter_build/lst
 mkdir -p mounter_build/obj
-make $1
+make -j$(nproc) $1
 cd ..
